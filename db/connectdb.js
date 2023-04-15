a4b0235f62e3a09e8ece35f50fe8cfd0aad0d35f
Vishal0422 <vssikarwar0422@gmail.com>
2023-04-15 20:32:32 +0530

    hello

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-// con = "mongodb+srv://vssikarwar:vssikarwar0422@cluster0.qxjye6c.mongodb.net/Bloggerweb?retryWrites=true&w=majority"
+const url = "mongodb+srv://vssikarwar:vssikarwar0422@cluster0.qxjye6c.mongodb.net/Blog_website?retryWrites=true&w=majority"
+//con = "mongodb+srv://vssikarwar:vssikarwar0422@cluster0.qxjye6c.mongodb.net/Bloggerweb?retryWrites=true&w=majority"
 const connectDB =()=>{
-    return mongoose.connect('mongodb://localhost:27017/blog_website')
-    return mongoose.connect(con)
+    //return mongoose.connect('mongodb://localhost:27017/blog_website')
+    return mongoose.connect(url)
     .then(()=>{
         console.log('Connection successful')
     })
